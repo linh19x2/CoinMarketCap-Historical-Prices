982bd668392f43189a30995c247bae1e7c6a4e66
linh19x2 <143103212+linh19x2@users.noreply.github.com>
2024-03-12 20:00:52 +0700

    multi_crypto_history.py

--- crypto_history.py
+++ crypto_history.py
@@ -9,7 +9,6 @@ import csv
 import sys
 from time import sleep
 
-
 def CoinNames():
     """Gets ID's of all coins on cmc"""
 
@@ -28,56 +27,51 @@ def gather(startdate, enddate, names):
         names = CoinNames()
 
     for coin in names:
-        sleep(10)
-        r  = requests.get("https://coinmarketcap.com/currencies/{0}/historical-data/?start={1}&end={2}".format(coin, startdate, enddate))
+        sleep(10)  # Be polite to the server
+        r = requests.get(f"https://coinmarketcap.com/currencies/{coin}/historical-data/?start={startdate}&end={enddate}")
         data = r.text
         soup = BeautifulSoup(data, "html.parser")
-        table = soup.find('table', attrs={ "class" : "table"})
+        table = soup.find('table', attrs={"class": "table"})
 
-        #Add table header to list
+        # Add table header to list
         if len(historicaldata) == 0:
             headers = [header.text for header in table.find_all('th')]
             headers.insert(0, "Coin")
 
         for row in table.find_all('tr'):
             currentrow = [val.text for val in row.find_all('td')]
-            if(len(currentrow) != 0):
+            if len(currentrow) != 0:
                 currentrow.insert(0, coin)
-            historicaldata.append(currentrow)
+                historicaldata.append(currentrow)
 
-        print("Coin Counter -> " + str(counter), end='\r')
+        print(f"Coin Counter -> {counter}", end='\r')
         counter += 1
+
     return headers, historicaldata
 
-def _gather(startdate, enddate):
+def _gather(startdate, enddate, names):
     """ Scrape data off cmc"""
-
-    if(len(sys.argv) == 3):
-        names = CoinNames()
-    else:
-        names = [sys.argv[3]]
-
     headers, historicaldata = gather(startdate, enddate, names)
-
     Save(headers, historicaldata)
 
 def Save(headers, rows):
-
-    if(len(sys.argv) == 3):
-        FILE_NAME = "HistoricalCoinData.csv"
-    else:
-        FILE_NAME = sys.argv[3] + ".csv"
-
-    with open(FILE_NAME, 'w') as f:
+    filename = "HistoricalCoinData.csv" if len(sys.argv) <= 3 else f"{sys.argv[3]}.csv"
+    with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(headers)
         writer.writerows(row for row in rows if row)
     print("Finished!")
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python crypto_history.py startdate enddate [coin1,coin2,...]")
+        sys.exit(1)
 
     startdate = sys.argv[1]
     enddate = sys.argv[2]
+    if len(sys.argv) > 3:
+        crypto_names = sys.argv[3].split(',')  # Split the third argument into a list
+    else:
+        crypto_names = []  # If no cryptocurrencies are specified, use an empty list to scrape all
 
-    _gather(startdate, enddate)
-
+    _gather(startdate, enddate, crypto_names)
